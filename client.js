b2a15cb523e8ce046ce6fe9c9ea8c457d4683c7c
Scott Roberts <sroberts.kia@gmail.com>
2019-07-31 15:15:15 +0000

    added connect and username, tested moves

--- client.js
+++ client.js
@@ -10,16 +10,33 @@ const connect = function() {
   });
   conn.setEncoding('utf8'); 
 
+   conn.on('connect', () => {
+     console.log("Successfully connected to game server");
+     conn.write("Name: SDR");
+   });
+
+  // Test Moves
+  //  conn.on('connect', () => {
+  //   setTimeout(() => {
+  //     conn.write("Move: up");
+  //   }, 1000);
+    
+  //   setTimeout(() => {
+  //     conn.write("Move: right");
+  //   }, 1500);
+  // });
+
+
+
   conn.on('data', (data) => {
     console.log('Message from server: ', data)
   });
   // interpret incoming data as text
  
 
+
   return conn;
 }
-
-
 
 
 
